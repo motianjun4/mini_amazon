fbf96e0150833969a8e37d0e97119b3d8bee4014
motianjun4 <motianjun4@gmail.com>
2022-03-14 22:57:35 -0400

    modify: db and object storage configuration

--- libs/my_minio.py
+++ libs/my_minio.py
@@ -1,3 +1,3 @@
 from minio import Minio
 
-minio_client = Minio("localhost:9000", access_key="minioadmin", secret_key="minioadmin", secure=False)
+minio_client = Minio("vcm.tinchun.top:9000", access_key="minioadmin", secret_key="minioadmin", secure=False)

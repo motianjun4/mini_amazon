b1d6c1d00754ff7fea6e8a265fad9cd322764897
Druig Zhang <97260309+vn-nv@users.noreply.github.com>
2022-03-14 23:26:41 -0400

    Update index.py
    bug fixed: no request for cart if no current user

--- app/index.py
+++ app/index.py
@@ -1,7 +1,6 @@
 from flask import render_template
 from flask_login import current_user
 import datetime
-
 from .models.product import Product
 from .models.cart import Cart
 from .models.purchase import Purchase
@@ -14,13 +13,15 @@ bp = Blueprint('index', __name__)
 def index():
     # get all available products for sale:
     products = Product.get_all(True)
-    cart_items = Cart.get_all_by_uid(current_user.id)
-    cart_cnt = Cart.get_count(current_user.id)
     # find the products current user has bought:
     if current_user.is_authenticated:
+        cart_items = Cart.get_all_by_uid(current_user.id)
+        cart_cnt = Cart.get_count(current_user.id)
         purchases = Purchase.get_all_by_uid_since(
             current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
     else:
+        cart_items = None
+        cart_cnt = None
         purchases = None
     # render the page by adding information to the index.html file
     return render_template('index.html',

e75283b55c9da3ff6af01417778cc237cbc33f98
dingzhou <wangdingzhou@wangdinzhousMBP.(none)>
2022-03-28 19:27:36 -0400

    create_table

--- app/inventorys.py
+++ app/inventorys.py
@@ -1,25 +1,25 @@
-from cmath import e
-from flask import render_template, redirect, request, url_for
+# from cmath import e
+# from flask import render_template, redirect, request, url_for
 
-import datetime
+# import datetime
 
-from numpy import product
-from app.models.inventory import Inventory
+# from numpy import product
+# from app.models.inventory import Inventory
 
-from app.utils.json_response import ResponseType, json_response
+# from app.utils.json_response import ResponseType, json_response
 
-from .models.purchase import Purchase
-from .models.account import Account
-from .models.user import User
+# from .models.purchase import Purchase
+# from .models.account import Account
+# from .models.user import User
 
-from flask import render_template, redirect, url_for, flash, request
-from werkzeug.urls import url_parse
-from flask_login import login_user, logout_user, current_user, login_required
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+# from flask import render_template, redirect, url_for, flash, request
+# from werkzeug.urls import url_parse
+# from flask_login import login_user, logout_user, current_user, login_required
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, IntegerField, PasswordField, BooleanField, SubmitField
+# from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
-current_user:User
+# current_user:User
 
 # from flask import Blueprint
 # bp = Blueprint('inventorys', __name__)
